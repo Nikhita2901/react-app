4c2968f1db99e873d2c61c8030fca0022e0e489c
Nikhita2901 <nikhita.s2901@gmail.com>
2020-09-23 02:15:58 +0530

    Added Hello.js

---
+++ src/Components/Hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Hello extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to React</h1>
+            </div>
+        )
+    }
+}
+
+export default Hello
