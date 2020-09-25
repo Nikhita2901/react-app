467506d15db86219fbe0d0c4c2efa96c4fcb2e7a
Nikhita2901 <nikhita.s2901@gmail.com>
2020-09-25 10:27:34 +0530

    Higher order components practice

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hello from './Components/Hello';
+import ClickCounter from './Components/ClickCounter';
+import HoverCounter from './Components/HoverCounter';
 
 function App() {
   return (
     <div className="App">
-      <Hello></Hello>
+      {/* <Hello></Hello> 
+      <ClickCounter/>*/}
+      <HoverCounter></HoverCounter>
+      <ClickCounter name='Nikhita'></ClickCounter>
     </div>
   );
 }
